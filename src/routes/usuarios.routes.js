35843f5e47cd60dcf7507d44ed426be5f7ae3118
Julio-Martel <julioramdom123@gmail.com>
2026-03-24 19:53:20 -0300

    fix: agregar nuevo usuario

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -5,10 +5,10 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const usuariosController = require('../controllers/usuarios.controller');
 
 router.get('/', usuariosController.mostrarTodosIdUsuario);
-router.post('/', authMiddleware.validarUsuario, usuariosController.realizarRegistro);
+router.post('/', authMiddleware.validarRegistro, usuariosController.realizarRegistro);
 //CORREGIR ESTO, SE MEZCLARON LOS METODOS JUNTO CON SUS FUNCIONES ADECUADAS
-router.put('/', authMiddleware.validarRegistro, usuariosController.realizarRegistro);
-router.patch('/:id', authMiddleware.validarId, usuariosController.modificarUsuario);
+/*router.put('/', authMiddleware.validarRegistro, usuariosController.realizarRegistro);
+router.patch('/:id', authMiddleware.validarId, usuariosController.modificarUsuario);*/
 
 
 module.exports = router;

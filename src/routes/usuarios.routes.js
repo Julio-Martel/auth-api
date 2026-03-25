9e6678a4c34638ec96fed05c0d855ee13ec2666d
Julio-Martel <julioramdom123@gmail.com>
2026-03-25 16:16:00 -0300

    refactor: codigo comentado

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -7,8 +7,8 @@ const usuariosController = require('../controllers/usuarios.controller');
 router.get('/', usuariosController.mostrarTodosIdUsuario);
 router.post('/', authMiddleware.validarRegistro, usuariosController.realizarRegistro);
 //CORREGIR ESTO, SE MEZCLARON LOS METODOS JUNTO CON SUS FUNCIONES ADECUADAS
-/*router.put('/', authMiddleware.validarRegistro, usuariosController.realizarRegistro);
-router.patch('/:id', authMiddleware.validarId, usuariosController.modificarUsuario);*/
+router.put('/', authMiddleware.validarRegistro, usuariosController.realizarRegistro);
+/*router.patch('/:id', authMiddleware.validarId, usuariosController.modificarUsuario);*/
 
 
 module.exports = router;
